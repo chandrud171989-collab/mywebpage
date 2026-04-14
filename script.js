0a72a20b8925d01b84dce34e6c02ef3f3cabc3de
chandrud171989-collab <chandru.d171989@gmail.com>
2026-04-14 23:52:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<!-- Add before </body> in index.html -->
+<script>
+  document.querySelector("h1").addEventListener("click", function () {
+    alert("Hello! You clicked the heading.");
+  });
+</script>
